7301660731cd38b68e0fee8d26ab221fc0de42d0
Matthew Piskorz <matthewpiskorz@gmail.com>
2014-08-14 17:00:09 -0600

    add phpunit to vm

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -49,5 +49,7 @@ ln -fs "../app/assets/css" "css"
 ln -fs "../app/assets/js" "js"
 ln -fs "../app/assets/fonts" "fonts"
 
-
+wget https://phar.phpunit.de/phpunit.phar
+chmod +x phpunit.phar
+sudo mv phpunit.phar /usr/local/bin/phpunit
 
